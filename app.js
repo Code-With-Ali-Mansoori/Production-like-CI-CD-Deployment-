82581eaba83d3a6de41728412df7ccac0322c9a4
Ali Mansoori <aliahmedmansoori090@gmail.com>
2026-07-20 22:04:58 +0530

    Check

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+  res.send('Hello from Express!, Checking This!');
 });
 
 app.get('/health', (req, res) => {
